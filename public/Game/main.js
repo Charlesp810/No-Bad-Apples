4f1370b329d7d593ad03115cafb5820283659c2d
Charlies Pan <Cp81089@gmail.com>
2020-06-26 00:09:46 -0400

    feat:gameover working

--- public/Game/main.js
+++ public/Game/main.js
@@ -25,32 +25,64 @@ let goodApple
 let badApple
 let background
 let scoreText
+let gameOverText
 let score = 0
 let ground
+let heart
+let heart2
+let heart3
+let youWin
+let lives = 3
 
 function preload() {
     this.load.image('background', '../images/apple-tree.png')
     this.load.image('ground', '../images/ground.png')
     this.load.image('boy', '../images/boy.png')
+    this.load.image('heart', '../images/heart.png')
     this.load.spritesheet('goodApple', '../images/mini-apple.png', {
-        frameWidth: 100,
-        frameHeight: 100,
+        frameWidth: 90,
+        frameHeight: 90,
         spacing: 1
     })
-    // this.load.spritesheet('badApple', '../images/mini-bad-apple/png')
+    this.load.spritesheet('badApple', '../images/mini-bad-apple.png', {
+        frameWidth: 90,
+        frameHeight: 90,
+        spacing: 1
+    })
 }
 
 function create() {
     background = this.add.image(0, 0, 'background').setOrigin(0).setScale(0.85)
+
     ground = this.physics.add.staticSprite(600, 675, 'ground')
 
     scoreText = this.add.text(16, 16, 'Score: 0', {
         fontSize: '20px',
         fill: '#000'
     })
+    gameOverText = this.add.text(550, 300, 'GAME OVER', {
+        fontSize: '125px',
+        fill: '#FF0000'
+    })
+    gameOverText.setOrigin(0.5)
+    gameOverText.visible = false
+
+
+
+    youWin = this.add.text(550, 300, "(>'.')> YOU WIN !!<('.'<)", {
+        fontSize: '75px',
+        fill: '#00FFFF'
+    })
+    youWin.setOrigin(0.5)
+    youWin.visible = false
+
+    heart = this.add.image(25, 50, 'heart')
+
+    heart2 = this.add.image(55, 50, 'heart')
+
+    heart3 = this.add.image(85, 50, 'heart')
 
     person = this.physics.add.sprite(500, 500, 'boy')
-
 
     this.input.keyboard.on('keydown_D', function (event) {
         person.x += 20
@@ -61,11 +93,12 @@ function create() {
     })
 
     goodApple = this.physics.add.group()
+
     goodApple.createMultiple({
         key: 'goodApple',
-        frame: Phaser.Utils.Array.NumberArray(0, 13),
+        frame: Phaser.Utils.Array.NumberArray(0, 11),
         randomFrame: true,
-        repeat: 1
+        repeat: 2
 
     })
 
@@ -77,20 +110,54 @@ function create() {
         child.setMaxVelocity(200)
     })
 
+    badApple = this.physics.add.group()
+
+    badApple.createMultiple({
+        key: 'badApple',
+        frame: Phaser.Utils.Array.NumberArray(0, 3),
+        randomFrame: true,
+        repeat: 1
+    })
+
+    badApple.children.iterate((child) => {
+        let y = Phaser.Math.Between(-200, -2000)
+        let x = Phaser.Math.Between(0, 1200)
+        child.setY(y)
+        child.setX(x)
+        child.setMaxVelocity(200)
+    })
+
+
     this.physics.add.collider(person, ground)
-    // this.physics.add.collider(person, goodApple, function (person, goodApple) {
-    //     goodApple.destroy()
-    // })
+    this.physics.add.collider(person, badApple, function (person, badApple) {
+        badApple.destroy()
+        lives -= 1
+    })
     this.physics.add.overlap(person, goodApple, collectFood)
+
 }
 
 function update(time, delta) {
     this.physics.world.wrap(goodApple, 300)
+    if (lives === 2) {
+        heart.destroy()
+    } else if (lives === 1) {
+        heart2.destroy()
+    } else if (lives <= 0) {
+        heart3.destroy()
+        this.physics.pause()
+        person.setTint('#FF0000')
+        gameOverText.visible = true
+    } else if (score === 30) {
+        this.physics.pause()
+        youWin.visible = true
+    }
 }
 
 function collectFood(person, goodApple) {
     goodApple.disableBody(true, true)
 
     score += 1
+
     scoreText.setText('Score: ' + score)
 }
